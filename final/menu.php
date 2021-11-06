77db2170de9264ca141202b2707ceb437be4a12a
Nilmarc <87557466+Nilmarc@users.noreply.github.com>
2021-11-06 17:58:55 -0600

    Add files via upload

---
+++ final/menu.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/estilos.css">
+    <title>Inicio</title>
+</head>
+<body>
+    <div class="contenedor">
+    <h3>Bienvenido formulario de Registro</h3>
+    
+    <form action="#" method="$_POST">
+        <br>
+        <br>
+    <input type="text" name="name" placeholder="Ingrese nombre de usuario" required/><br><br>
+    <input type="text" name="password" placeholder="Ingrese contraseña" required/><br><br>
+    <input type="text" name="persona" placeholder="Ingrese nombre de la persona" required/><br><br>
+    <input type="text" name="correo" placeholder="Ingrese su correo" required/><br><br>
+    <select name="select">
+    <option value="value1">Seleccione</option>
+    <option value="value2">Administrador</option>
+    <option value="value3">Usuario</option>
+    <option value="value4">Médico</option>
+    <option value="value5">Enfermera</option>
+    <option value="value6">Secretaria</option>
+    </select>
+    <br><br>
+    <p><input type="submit" /></p>
+
+    </form>
+    </div>
+</body>
+</html>
